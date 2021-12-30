8daa9a43e8893775c865b743cd2e48feae4ff02e
ycourses <cood_ip@hotmail.com>
2021-12-30 13:57:31 +0200

    jshint        add

--- src/core/application.js
+++ src/core/application.js
@@ -1,32 +1,31 @@
-import express, { request }  from "express";
+/*jshint esversion: 6 */
+import express, { request } from "express";
 import bodyParser from "body-parser";
 import { env } from "./env";
- export default class  Application {
-    constructor (){
-        this.prepareServer();
-    }
-    /**
-     * prepare this express server 
-     */
-    prepareServer(){
-        this.express=express();
-        this.express.use(bodyParser.urlencoded({
-            extended:true,
-        }));
-
-
-    }
-    /**
-     * Run the application 
-     */
-    run(){
-            this.express.get('/',(request,response)=>{
-                response.send("wellcome to express app ")
-            });
-            this.express.listen(process.env.PORT,function(){
-                console.log("server is ready ...😜 @ http://localhost:3000/")
-            });
-
+export default class Application {
+    constructor() {
+            this.prepareServer();
+        }
+        /**
+         * prepare this express server
+         */
+    prepareServer() {
+            this.express = express();
+            this.express.use(
+                bodyParser.urlencoded({
+                    extended: true,
+                })
+            );
+        }
+        /**
+         * Run the application
+         */
+    run() {
+        this.express.get("/", (request, response) => {
+            response.send("wellcome to express app ");
+        });
+        this.express.listen(process.env.PORT, function() {
+            console.log("server is ready ...😜 @ http://localhost:3000/");
+        });
     }
 }
- 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import application from "./core/application";
-let app =new application;
- app.run();
-
+let app = new application();
+app.run();
